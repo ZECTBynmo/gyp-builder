3b7ad1a917b2aeb1b4c899710999375c3c7b5a5e
ZECTBynmo <michael.vegeto@gmail.com>
2013-01-21 14:26:57 -0500

    Empty skeliton of a gyp file

---
+++ empytSkeliton.js
@@ -0,0 +1,26 @@
+var skeliton = {
+    'variables': {
+
+    },
+    'includes': [
+
+    ],
+    'target_defaults': {
+
+    },
+    'targets': [
+
+    ],
+    'conditions': [
+      ['OS=="linux"', {
+
+      }],
+      ['OS=="win"', {
+
+      }, { // OS != "win"
+
+      }],
+    ],
+}
+
+exports.skeliton = skeliton;
