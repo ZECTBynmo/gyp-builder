f7902073481e680330ebfe9668c691b13695fa93
ZECTBynmo <michael.vegeto@gmail.com>
2013-01-21 23:38:09 -0500

    Targets example

---
+++ examples/targets/skelitons.js
@@ -0,0 +1,16 @@
+var skeliton = {
+    'variables': {
+
+    },
+    'includes': [
+
+    ],
+    'target_defaults': {
+
+    },
+    'targets': [
+
+    ],
+}
+
+exports.skeliton = skeliton;
---
+++ examples/targets/targets.js
@@ -0,0 +1,20 @@
+var ares = require("ares").ares;
+
+var builder = require("../../gyp-builder").createBuilder( "test", false );
+
+var firstTarget = {
+	'target_name': 'testTarget1',
+  	'type': '<(library)'
+}
+
+var secondTarget = {
+	'target_name': 'testTarget2',
+  	'type': 'executable'
+}
+
+builder.setTarget( firstTarget );
+builder.setTarget( secondTarget );
+
+// Write the gyp file to disk
+// With no arguments, this creates the file in the current working directory
+builder.writeGypFile( __dirname + "/test/" );
