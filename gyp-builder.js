a9c29c9a126786f0af8859d3fa5254a9e6bce546
ZECTBynmo <michael.vegeto@gmail.com>
2013-01-21 23:38:38 -0500

    Add some abilities to manipulate targets directly, since they'll probably get the most use

--- gyp-builder.js
+++ gyp-builder.js
@@ -43,17 +43,52 @@ Builder.prototype.setSkeliton = function( skeliton ) {
 
 
 //////////////////////////////////////////////////////////////////////////
-//  Replace the current value of some part of the gyp object
-Builder.prototype.set = function( key, value ) {
-
+// Replace the current value of some part of the gyp object
+// NOTE: This function's arguments are parsed within Builder.getTargetEntry()
+Builder.prototype.set = function( /*key1, key2...keyn, value*/ ) {
+	// Grab the piece of the git object that we're trying to access
+	this.modifyGitObject( arguments, true );
 } // end set()
 
 
 //////////////////////////////////////////////////////////////////////////
-//  Add a value to some part of the gyp object
-Builder.prototype.add = function( key, value ) {
-
+// Add a value to some part of the gyp object
+// NOTE: This function's arguments are parsed within Builder.getTargetEntry()
+Builder.prototype.add = function( /*key1, key2...keyn, value*/ ) {
+	// Grab the piece of the git object that we're trying to access
+	this.modifyGitObject( arguments, false );
 } // end add()
+
+
+//////////////////////////////////////////////////////////////////////////
+// Returns a copy of the target matching some name
+Builder.prototype.getTarget = function( name ) {
+	var targets = this.gypObject["targets"];
+
+	for( var iTarget=0; iTarget<targets.length; ++iTarget ) {
+		if( targets[iTarget]["target_name"] == name )
+			return targets[iTarget];
+	}
+} // end getTarget()
+
+
+//////////////////////////////////////////////////////////////////////////
+// Returns a copy of the target matching some name
+Builder.prototype.setTarget = function( target ) {
+	var targets = this.gypObject["targets"],
+		hasFoundTarget = false;
+
+	for( var iTarget=0; iTarget<targets.length; ++iTarget ) {
+		if( targets[iTarget]["target_name"] == target["target_name"] ) {
+			targets[iTarget] = target;
+			hasFoundTarget
+		}
+	}
+
+	// If the target didn't already exist, push a new one onto our targets
+	if( !hasFoundTarget )
+		this.gypObject["targets"].push( target );
+} // end setTarget()
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -99,11 +134,52 @@ Builder.prototype.writeGypFile = function( dir ) {
 	        writeFile( path );
 	    }
 	} catch( error ) {
+		log( "Creating directory: " + dir );
+
 		// Create the directory
-		fs.mkdir( __dirname + "/" + this.name, function(error) {
+		fs.mkdir( dir, function(error) {
 			if( error === null ) {
 				writeFile( path );
+			} else { 
+				log(error); 
 			}
 		});
-	}	
+	}
 } // end writeGypFile()
+
+
+//////////////////////////////////////////////////////////////////////////
+// Traverses the gyp object tree and returns a reference to
+// the intended value
+Builder.prototype.modifyGitObject = function( arguments, shouldOverwrite ) {
+	// The last argument is the value we're assigning, so the depth
+	// of the value inside the object tree is number of args minus 1
+	var depth = arguments.length - 1,
+		value = arguments[arguments.length-1];
+
+	switch(arguments.length) {
+	case 1:
+	  	log( "Gyp object key not provided" );
+	  	break;
+
+	case 2:
+		if( shouldOverwrite || this.gypObject[ arguments[0] ] === undefined ) {
+			this.gypObject[ arguments[0] ] = value;
+		} else {
+			this.gypObject[ arguments[0] ].push( value );
+		}
+		break;
+
+	case 3:
+		if( shouldOverwrite || this.gypObject[ arguments[0] ][ arguments[1] ] === undefined ) {
+			this.gypObject[ arguments[0] ][ arguments[1] ] = value;
+		} else {
+			this.gypObject[ arguments[0] ][ arguments[1] ].push( value );
+		}
+		break;
+
+	default:
+	  	log( depth + " arguments not handled. Edit Builder.getTargetEntry to add another override");
+	  	return;
+	}
+} // end modifyGitObject()
