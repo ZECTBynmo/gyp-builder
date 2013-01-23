9f444c4fc31a5c48c99b49191112ecf1cf238446
ZECTBynmo <michael.vegeto@gmail.com>
2013-01-23 01:11:00 -0500

    Delete the basic test (which didn't work anyway)

--- test.js
+++
@@ -1,5 +0,0 @@
-var builder = require("./gyp-builder").createBuilder( "test", false );
-
-// Write the gyp file to disk
-// With no arguments, this creates the file in the current working directory
-builder.writeGypFile();
